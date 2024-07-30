bc50f8781d426e702e2231b1dba31744a1abc53f
Leonardo Aquino <laquino@soaint.com>
2024-07-30 15:45:30 -0500

    Feat: uso de los features.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,99 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InferencePresenter } from './tipos/a_inference.presenter';
+import { DefinidedNs } from './tipos/b_definided.ns';
+import { DefinidedPresenter } from './tipos/b_definided.presente';
+import { StructuralSystemPresenter } from './tipos/c_structual_system.presenter';
+import { NoIferPresenter } from './NoIfer/noIfer.presenter';
+import { GroupByPersenter } from './group/groupBy.presenter';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [
+    InferencePresenter, DefinidedPresenter, StructuralSystemPresenter,
+    NoIferPresenter,
+    GroupByPersenter
+  ]
 })
-export class AppComponent {
-  title = 'typescript-basic';
+export class AppComponent implements OnInit {
+
+  constructor(
+    public inferenceP: InferencePresenter,
+    public definidedP: DefinidedPresenter,
+    public structualSystemP: StructuralSystemPresenter,
+    public noIferP: NoIferPresenter,
+    public groupByP: GroupByPersenter
+  ) {}
+
+  title = 'Typescript';
+  listuser: DefinidedNs.User[] = [];
+
+  ngOnInit(): void {
+    this.init();
+  }
+
+  init(): void {
+    this.inferenceVariables();
+    this.setearUsuarioClase();
+    this.setuserExercise();
+    this.useStructureSystem();
+    this.useNoIfer();
+    this.getDataGroupBy();
+  }
+
+  inferenceVariables(): void {
+    console.log('Tipo de inferencia::',this.inferenceP.task);
+  }
+
+  setuserExercise(): void {
+    this.definidedP.addUser({id: 33, name: 'Leonardo'});
+    this.definidedP.addUser({id: 23, name: 'Pamelita'});
+    this.definidedP.addUser({id: 31, name: 'Ericka'});
+
+    console.table(this.definidedP.getAllUsers());
+  }
+
+  setearUsuarioClase(): void {
+    const user: DefinidedNs.User = {
+      id: 1,
+      name: 'Leonardo'
+    }
+    const UserCuenta: DefinidedNs.User = new DefinidedNs.UserAccount("pepito", 70);
+    console.log('UserCuenta::', UserCuenta);
+  }
+
+
+  useStructureSystem(): void {
+    const point = {x:12, y:26};
+    this.structualSystemP.logPoint(point);
+
+    const point3 = {x:15, y:26, z:89}
+    this.structualSystemP.logPoint(point3);
+
+    const color =  {hex: "#187ABF"};
+    // this.structualSystemP.logPoint(color);
+  }
+
+  useNoIfer(): void {
+    //Sin Ifer
+    this.noIferP.createTrafficLight__without__NoIfer(['rojo', 'amarillo', 'verde'], 'azul');
+
+    //Sin Ifer:
+    // this.noIferP.createTrafficLight__without__NoIfer__By_param(['rojo', 'amarillo', 'verde'], 'azul');
+    //Esto funciona, pero es un poco extraño porque D probablemente no se usará en ningún otro lugar del método
+    //para createTrafficLight__without__NoIfer__By_param().
+    //Si bien no está mal en este caso, usar un parámetro de tipo solo una vez suele ser un error de codeSmell.
+
+    //Con Ifer:
+    // this.noIferP.createTrafficLight__with__NoIfer(['rojo', 'amarillo', 'verde'], "verde");
+  }
+
+  getDataGroupBy(): void {
+    this.groupByP.getGroup();
+  }
+
 }
